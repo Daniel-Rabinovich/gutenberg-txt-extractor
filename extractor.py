2e84bfaef7b49bf8435c1063d31ad5d50815e93e
Daniel Rabinovich <dannyrabinovich@gmail.com>
2022-03-21 14:42:23 +0200

    code refactoring

--- extractor.py
+++ extractor.py
@@ -1,24 +1,11 @@
 import requests, csv, re
 
-
-# TODO
-# [X] read csv file
-# [X] filter unneeded book
-# [V] extract books
-
-# ****** extra info for this script ******
-# gutenberg mirror list: https://www.gutenberg.org/MIRRORS.ALL
-#
-# high speed mirror link: https://gutenberg.pglaf.org/
-#
-# All eBooks are arranged by eBook number, with a unique subdirectory
-# based on that eBook number (starting with numbers 1-9).  To get to the
-# files for a particular eBook, you need to "drill down" a few
-# subdirectory levels.  For example, eBook #11 is in this subdirectory:1/11
-# and eBook #12714 is in this subdirectory: 1/2/7/1/12714
+# *****************************************
+#  Helper functions
+# *****************************************
 
 def parse_id(book_id):
-    """ turn int: 1234 into str: 1/2/3/1234 """
+    """ turn 1234 into 1/2/3/1234 """
     book_id_copy = book_id
     book_id = str(book_id)
     book_id = [c for c in book_id]
@@ -31,30 +18,61 @@ def make_link(path, tag):
     
 def extract_txt_links(path, html_data):
     """ find all the possible *.txt links in the book's page and returns them as a list"""
-    links = []
     results = re.findall("<a href=\".+\.txt\">", html_data)
-    for tag in results:
-        links.append(make_link(path, tag))
-    return links
-    
-def get_book(book_id):
-    """ fetch book as string and return it """
-    query = "https://gutenberg.pglaf.org/{}".format(parse_id(book_id))
-    response = requests.get(query)
-    links = extract_txt_links(query, response.text)
-    
-    # for now get only the first link
-    print(links[0])
-    # TODO add utf-8 check
-    return requests.get(links[0]).text
+    return [make_link(path, tag) for tag in results]
         
-def save_book(folder, book_id):
+def save_book(folder, book_txt, book_id):
     """ save the book to txt file in folder"""
-    f_book = open("{}/{}.txt".format(folder,book_id), "w")
-    f_book.write(get_book(book_id))
+    f_book = open("{}/{}.txt".format(folder,book_id), "w", encoding='utf-8')
+    f_book.write(book_txt)
     f_book.close()
 
-# testing
-# if you run this script manually create "t" folder
-for i in range(101,120):
-    save_book("t",i)
+def get_full_book_path(mirror, book_id):
+    """ combine mirror url with book path """
+    return "{}/{}".format(mirror,parse_id(book_id))
+
+def get_txt_links_list(book_url):
+    """ return all the urls containig .txt"""
+    response = requests.get(book_url)
+    return extract_txt_links(book_url, response.text)
+    
+def pull_book(url):
+    """ return the book as a string """
+    return requests.get(url).text
+
+def choose_url(arr):
+    """ choose the right link containing the book """
+    return arr[0]
+
+
+# *****************************************
+#  Main
+# *****************************************
+
+
+# handle pulling books 
+mirror = "https://gutenberg.pglaf.org"
+main_txt_dir = "books" 
+
+for book_id in range(101,120):
+
+    print("starting book #{}".format(book_id))
+    
+    # parse book url
+    book_url = get_full_book_path(mirror, book_id)
+    
+    # pull txt links from page
+    txt_urls = get_txt_links_list(book_url)
+    
+    # choose url (for now choose the first one)
+    chosen_txt_url = choose_url(txt_urls)
+    
+    # pull book
+    book_txt = pull_book(chosen_txt_url)
+    
+    # save the book 
+    save_book(main_txt_dir, book_txt, book_id)
+    
+    
+        
+
