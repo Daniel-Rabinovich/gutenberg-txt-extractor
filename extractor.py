a41574d355bdb32b2250f0700cfd292abfdc4419
Daniel Rabinovich <dannyrabinovich@gmail.com>
2022-03-22 18:46:09 +0200

    added csv handling and book filtering

--- extractor.py
+++ extractor.py
@@ -4,6 +4,65 @@ import requests, csv, re
 #  Helper functions
 # *****************************************
 
+def csv_to_dict(path="pg_catalog.csv"):
+    catalog = dict()
+    with open(path, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+        for row in csv_reader:
+            catalog[row['Text#']] = {
+                'language':row['Language'],
+                'subjects':row['Subjects'],
+            }
+            line_count += 1
+    return catalog
+
+def remove_non_english_books(books_dict):
+    keys_to_remove = []
+    for id in books_dict.keys():
+        if books_dict[id]["language"] != "en":
+            keys_to_remove.append(id)
+
+    for key in keys_to_remove:
+        books_dict.pop(key, None)
+
+def remove_unwanted_categories(books_dict):
+    keys_to_remove = []
+    for id in books_dict.keys():
+        if not check_good_category(books_dict, id):
+            keys_to_remove.append(id)
+
+    for key in keys_to_remove:
+        books_dict.pop(key, None)
+    
+def check_good_category(books_dict, book_id):
+    if str(book_id) in books_dict.keys():
+        book = books_dict.get(str(book_id))
+        subjects = book.get("subjects")
+        categories_found = []
+
+        #extract categories
+        rows = subjects.split(";")
+        for i in rows:
+            tmp = i.split(" -- ")
+            for j in tmp:
+                categories_found.append(j.strip().lower())
+
+        categories_found = set(categories_found)
+
+        for cat in categories_found:
+            if cat in ["fiction"]:
+                return True
+        return False
+    else:
+        return False
+
+def get_book_ids_from_dict(books_dict):
+    ids = []
+    for key in books_dict.keys():
+        ids.append(key)
+    return ids
+
 def parse_id(book_id):
     """ turn 1234 into 1/2/3/1234 """
     book_id_copy = book_id
@@ -49,10 +108,17 @@ def choose_url(arr):
 #  Main
 # *****************************************
 
+# read csv with book data
+books = csv_to_dict()
+remove_non_english_books(books)
+remove_unwanted_categories(books)
+
+# get only the ids 
+books_ids = get_book_ids_from_dict(books)
 
 # handle pulling books 
 mirror = "https://gutenberg.pglaf.org"
-main_txt_dir = "books" 
+main_txt_dir = "books"
 
 for book_id in range(101,120):
 
@@ -74,5 +140,4 @@ for book_id in range(101,120):
     save_book(main_txt_dir, book_txt, book_id)
     
     
-        
-
+    
