52afb65522204a559ab98eb11066a9279b2f9ae7
Daniel Rabinovich <dannyrabinovich@gmail.com>
2022-03-29 12:14:28 +0300

    progress on db creation

--- db_creator.py
+++ db_creator.py
@@ -1,11 +1,11 @@
 import sqlite3, re, csv, sys, math, os
-
+from gutenberg_cleaner import super_cleaner
+import nltk
+from nltk.corpus import stopwords
 
 # this step comes after you downloaded all the books 
 # each books downloaded will have and entry in the sqlite db
 
-# 1. get file names 
-# 2. loop file names and create entry for each book using catalog + gender
 
 # *************************
 # helper classes
@@ -78,7 +78,8 @@ class Catalog(Singleton):
                 self.__dict[line[0]] = {
                     "title": line[3],
                     "year": line[2].split("-")[0],
-                    "authors": line[5]}
+                    "authors": line[5],
+                    "type": line[1]}
                 
     def get_book(self, id):
         if id in self.__dict.keys():
@@ -93,24 +94,30 @@ class BookMetadata:
     """ this class holds the metadata of a single book,
     also generates the gender and estimated year automatically """
     
-    def __init__(self, name_gender_singelton, b_id, year, title, authors, filepath):
-        self.__book_id = b_id
+    def __init__(self, name_gender_singelton, book_id, year, title, authors, type, filepath):
+        self.__book_id = book_id
         self.__year = year
         self.__title = title
         self.__authors = authors
         self.__gender = "X"
         self.__filepath = filepath
+        self.__type = type
         self.__name_gender = name_gender_singelton
         
         self.__update_estimated_year()
         self.__update_gender()
         
     def __repr__(self):
-        return f"({self.__book_id}:{self.__title}:{self.__authors}:{self.__gender}:{self.__year})"
+        return f"""(
+            {self.__book_id},
+            {self.__title},
+            {self.__authors},
+            {self.__gender},
+            {self.__year}
+            )"""
         
     def __extract_author_list(self):
         return [author.strip() for author in self.__authors.split(";")]
-    
     
     def __update_estimated_year(self):
         authors = self.__extract_author_list()
@@ -122,7 +129,7 @@ class BookMetadata:
             elif amount > 1:
                 sum = 0
                 for i in range(amount):
-                    sum = sum + int(found_years[i])
+                    sum += int(found_years[i])
                 self.__year = math.floor(sum/amount)
     
     def __update_gender(self):
@@ -150,6 +157,9 @@ class BookMetadata:
     def get_authors(self):
         return self.__authors
     
+    def get_type(self):
+        return self.__type
+    
     def get_title(self):
         return self.__title
     
@@ -159,15 +169,22 @@ class BookMetadata:
 class Library:
     """ this class holds a dictionary of BooksMetadata object """
     
-    def __init__(self, path, catalog, name_gender):
+    def __init__(self, path, catalog_path, name_gender_path, db_path):
         self.__books = {}
-        self.__catalog = catalog
-        self.__name_gender = name_gender
-        self.__read_book_paths(path)
+        self.__catalog = Catalog(catalog_path)
+        self.__name_gender = NameGender(name_gender_path)
+        self.__db_path = db_path 
+        self.__read_books_metadata(path)
         
         self.__remove_no_gender_books()
+        self.__remove_non_text_books()
         
-    def __read_book_paths(self, path):
+        nltk.download('punkt')
+        nltk.download('stopwords')
+        self.__stop_words = stopwords.words("english")
+
+        
+    def __read_books_metadata(self, path):
         for dir, _, filenames in os.walk(path):
             for filename in filenames:
                 book_id = filename.split(".")[0]
@@ -177,6 +194,7 @@ class Library:
                     book_id, book["year"], 
                     book["title"], 
                     book["authors"],
+                    book["type"],
                     f"{dir}{filename}")
         
     def __remove_no_gender_books(self):
@@ -186,7 +204,80 @@ class Library:
                 books_to_remove.append(key)
         for book in books_to_remove:
             self.__books.pop(book)
-        
+    
+    def __remove_non_text_books(self):
+        books_to_remove = []
+        for key in self.__books.keys():
+            if self.__books[key].get_type() != "Text":
+                books_to_remove.append(key)
+        for book in books_to_remove:
+            self.__books.pop(book)
+    
+    def start(self):
+        db = Save(self.__db_path)
+        books_ids = self.get_books().keys()
+        books_inserted = 0
+        total_books = len(books_ids)
+        for key in books_ids:
+            book = self.get_book(key)
+            print(f"({books_inserted}/{total_books}) inserting book #{key}")
+            proccessed_book = []
+            
+            # insert metadata
+            values =[book.get_id(), 
+                     book.get_title(), 
+                     book.get_authors(), 
+                     book.get_gender(), 
+                     book.get_year()]
+            db.insert("Metadata", values)
+            
+            # insert book content
+            text = self.__read_text_from_file(key)
+            text = super_cleaner(text, 5,  600)
+            
+            text = text.lower()
+            text = re.sub("[\\r]*","",text)
+            text = text.split("\n\n")
+            
+            for i in range(len(text)):
+                text[i] = re.sub("\n+"," ",text[i])
+                text[i] = re.sub("[^a-z0-9\,\.\[\]\"\']"," ",text[i])
+                text[i] = text[i].strip()
+                text[i] = re.sub("\s+"," ",text[i])
+                if text[i] != "[deleted]":
+                    proccessed_book.append(text[i])
+            
+            para_id = 1 
+            for paragraph in proccessed_book:
+                values = [key,
+                        para_id,
+                        paragraph,
+                        self.__word_count(paragraph),
+                        self.__stop_words_count(paragraph)]
+                db.insert("Books", values)
+                para_id += 1
+            books_inserted +=1 
+            
+            if books_inserted % 20 == 0:
+                db.con.commit()
+        db.con.commit()
+        db.con.close()
+    def __word_count(self,paragraph):
+        tmp = paragraph.split(" ")
+        return len(tmp)
+
+    def __stop_words_count(self,paragraph):
+        count = 0
+        tmp = paragraph.split(" ")
+        for x in tmp:
+            if x in self.__stop_words:
+                count = count + 1
+        return count
+    
+    def __read_text_from_file(self, key):
+        f = open(self.get_book(key).get_filepath(),'r',encoding='utf8')
+        return f.read()
+    
     def get_books(self):
         return self.__books
 
@@ -200,20 +291,21 @@ class Save:
     
     def __init__(self, db_path):
         self.__path = db_path
+        self.con = sqlite3.connect(self.__path)
         self.__drop_tables()
         self.__create_db()
+       
         
         
     def __drop_tables(self):
-        con = sqlite3.connect(self.__path)
-        con.execute("DROP TABLE Metadata")
-        con.execute("DROP TABLE Books")
-        con.commit()
-        con.close()
+        self.con.execute("DROP TABLE Metadata")
+        self.con.execute("DROP TABLE Books")
+        self.con.commit()
+        print("removed old tables")
         
     def __create_db(self):
-        con = sqlite3.connect(self.__path)
-        con.execute("""
+        
+        self.con.execute("""
             create table if not exists "Metadata" (
             "book_id"           INTEGER NOT NULL,
             "title"             TEXT NOT NULL,
@@ -223,7 +315,7 @@ class Save:
             PRIMARY KEY("book_id")
             );
             """)
-        con.execute("""
+        self.con.execute("""
             create table if not exists "Books" (
             "book_id"           INTEGER NOT NULL,
             "paragraph_id"      INTEGER NOT NULL,
@@ -233,35 +325,37 @@ class Save:
             PRIMARY KEY("book_id","paragraph_id")
             );
             """)
-        con.commit()
-        con.close()
+        self.con.commit()
+        print("created Metadata,Books tables")
     
-    def insert_metadata(self, v):
-        con = sqlite3.connect(self.__path)
-        query = "INSERT INTO Metadata VALUES(?,?,?,?,?)"
-        con.execute(query,tuple(v))
-        con.commit()
-        con.close()
+    def insert(self, table, v):
+        query = f"INSERT INTO {table} VALUES(?,?,?,?,?)"
+        self.con.execute(query,tuple(v))
 
 # *************************
 # Main
 # *************************
 
 def main():
+    # all the paths to the correct files
     gender_csv_path = "csv/name_gender.csv"
     catalog_csv_path = "csv/pg_catalog.csv"
     books_dir_path = "books/"
     db_path = "data.db"
+        
+    # init object that holds all the correct books 
+    # (automatically filters them)
+    lib = Library(books_dir_path, catalog_csv_path, gender_csv_path, db_path)
     
-    gender = NameGender(gender_csv_path)
-    catalog = Catalog(catalog_csv_path)
-    lib = Library(books_dir_path, catalog, gender)
+    
     books = lib.get_books()
-    db = Save(db_path)
+    
+    count = 0
     for key in books.keys():
-        book = books[key]
-        values =[book.get_id(), book.get_title(), book.get_authors(), book.get_gender(), book.get_year()]
-        db.insert_metadata(values)
-
+        count += 1
+    print(f"There are {count} valid books")    
+    
+    lib.start()
+    
 if __name__ == "__main__":
     main()
